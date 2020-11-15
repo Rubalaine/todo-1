43b9dfdf11ec81c6d1d90cb58bca63354ecdb35d
Rubalaine <kelvenizzy@gmail.com>
2020-11-15 09:59:01 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation";
+import Main from "./Main";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Page3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Page3 = () => (
+  <div>
+    <h2>Page 2</h2>
+  </div>
+);
+
+export default Page3;
